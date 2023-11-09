a440d0a3c9c188bc7fa811245014a0d532873940
Andreas Anderberg <andreas.anderberg@u-blox.com>
2023-11-09 16:31:11 +0100

    Fix run_server target in objective4 Makefile

--- zephyr_server/run.sh
+++ zephyr_server/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Get the script directory
+SCRIPT_DIR=$(dirname "$0")
+
 PORT=$((${UID}+1024))
-qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -nic user,model=stellaris,hostfwd=tcp::${PORT}-:4242 -pidfile qemu.pid -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -kernel zephyr.elf
+qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -nic user,model=stellaris,hostfwd=tcp::${PORT}-:4242 -pidfile qemu.pid -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -kernel ${SCRIPT_DIR}/zephyr.elf
