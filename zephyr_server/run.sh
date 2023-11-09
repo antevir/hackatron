d87dafd706f70677607b73c6d47179cf0644bf48
Andreas Anderberg <andreas.anderberg@u-blox.com>
2023-11-09 16:27:59 +0100

    Add offset for qemu port for allowing UID < 1024

--- zephyr_server/run.sh
+++ zephyr_server/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -nic user,model=stellaris,hostfwd=tcp::${UID}-:4242 -pidfile qemu.pid -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -kernel zephyr.elf
+PORT=$((${UID}+1024))
+qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -nic user,model=stellaris,hostfwd=tcp::${PORT}-:4242 -pidfile qemu.pid -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -kernel zephyr.elf
